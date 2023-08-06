ba0ebc818bb281710250e987421645a65ccfdfef
Suriyaravichandran <rsuriya119@gmail.com>
2023-08-06 20:08:30 +0530

    update

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-#import packages
-
+import os
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO, emit, join_room, leave_room
-
-#create app & secure key
+from werkzeug.utils import secure_filename
+import MySQLdb
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
-#connect template our app
+mysql = MySQLdb.connect(host='localhost', user='root', password='', db='chat_app', autocommit=True)
 
 @app.route('/')
 def index():
@@ -20,12 +19,34 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.cursor()
+        cursor.execute('SELECT id, username FROM users WHERE username = %s AND password = %s', (username, password))
+        user = cursor.fetchone()
+        cursor.close()
+        if user:
+            session['user_id'] = user[0]
+            session['username'] = user[1]
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', message='Invalid credentials. Please try again.')
     return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.cursor()
+        cursor.execute('INSERT INTO users (username, password) VALUES (%s, %s)', (username, password))
+        cursor.close()
+        return redirect(url_for('login'))
+    return render_template('register.html')
 
 @app.route('/logout')
 def logout():
+    session.pop('user_id', None)
     session.pop('username', None)
     return redirect(url_for('login'))
 
@@ -33,6 +54,7 @@ def logout():
 def on_join(data):
     if 'username' not in session:
         return
+    user_id = session['user_id']
     username = session['username']
     room = data['room']
     join_room(room)
@@ -42,6 +64,7 @@ def on_join(data):
 def on_leave(data):
     if 'username' not in session:
         return
+    user_id = session['user_id']
     username = session['username']
     room = data['room']
     leave_room(room)
@@ -51,11 +74,11 @@ def on_leave(data):
 def handle_message(data):
     if 'username' not in session:
         return
+    user_id = session['user_id']
     username = session['username']
     room = data['room']
     message = data['message']
     emit('message', {'username': username, 'message': message}, room=room)
 
-#add host and port
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
